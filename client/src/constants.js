e39be07ed54dd95b4e6f2262bbcfc2c3c49d4d6e
Sourabh Shukla <sourabhshukla3.14@gmail.com>
2025-11-01 20:49:24 +0530

    added env file support

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,9 @@
-export const API_BASE_URL = "http://localhost:5001/api/v1";
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api/v1";
 // export const API_BASE_URL = "http://10.42.0.52:5001/api/v1";
 
-export const SOCKET_BASE_URL = "http://localhost:5001";
+export const SOCKET_BASE_URL =
+  import.meta.env.VITE_SOCKET_BASE_URL || "http://localhost:5001";
 
-export const FILE_BASE_URL = "http://localhost:5001";
+export const FILE_BASE_URL =
+  import.meta.env.VITE_FILE_BASE_URL || "http://localhost:5001";
